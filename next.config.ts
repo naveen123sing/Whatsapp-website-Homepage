bc618a351b8a5e59bf46d66e14b88a912611288b
gopal jangir <ns7921963@gmail.com>
2026-06-01 18:19:11 +0530

    add output export tag

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+   output: "export",
   allowedDevOrigins: ['192.168.200.42'],
+ 
   images: {
     qualities: [75, 95, 100],
+    unoptimized: true,
   },
 };
 
